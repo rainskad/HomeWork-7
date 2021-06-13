8daf4510c9a2772315278e6000b30ba887064a67
RainSkad <rainskad@gmail.com>
2021-06-13 16:26:35 +0200

    1 task

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,12 @@
-const ulCategories = qocument.querySelector('#categories');
-console.log(ulCategories);
+const itemsLi = document.querySelectorAll('.item');
+
+const numberOfCategories = itemsLi.length;
+console.log(`В списке ${numberOfCategories} категории`);
+
+itemsLi.forEach(item => {
+    console.log(`категория: ${item.querySelector('h2').textContent}`);
+    console.log(`Количество элементов: ${item.querySelectorAll('li').length}`);
+
+});
+
+
