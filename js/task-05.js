dbaf38f91ee72f81ce2319a0263759d1dc1935e4
RainSkad <rainskad@gmail.com>
2021-07-03 12:55:20 +0200

    task 5 done

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,16 @@
-task 5;
+const refs = {
+    input: document.querySelector('#name-input'),
+    nameLabel: document.querySelector('#name-output'),
+};
+
+refs.input.addEventListener('input', onInputChange);
+
+
+
+function onInputChange(event) {
+    if (refs.nameLabel.textContent = event.currentTarget.value) {
+        return refs.nameLabel.textContent
+    } else {
+       return refs.nameLabel.textContent = 'незнакомец';
+    };
+};
